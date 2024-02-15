ad6c28ce09f3e7ea82a3864dab97ae3eb1eff785
raj0987-alt <raj.tazimul2011@gmail.com>
2024-02-15 22:25:38 +0600

    Try First

---
+++ FizzBuzz/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,28 @@
+﻿int A = 51;
+List<string> list = new List<string>();
+int t = 0;
+for (int i = 0; i < A; i++)
+{
+    t = i + 1;
+    if(t%3 == 0 && t%5 == 0)
+    {
+        list.Add("FizzBuzz");
+    }
+    else if(t%3 == 0)
+    {
+        list.Add("Fizz");
+    }
+    else if(t%5 == 0) {
+        list.Add("Buzz");
+
+    }
+    else
+    {
+        list.Add(t.ToString());
+    }
+}
+
+for(int i = 0;i < list.Count;i++)
+{
+    Console.WriteLine(list[i]);
+}
