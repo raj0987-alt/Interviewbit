bb08acef73e6277f2e1fabb25dce4006676c5093
raj0987-alt <raj.tazimul2011@gmail.com>
2024-02-15 22:13:39 +0600

    Prime Number

---
+++ Prime Number/PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿int A = 100; 
+int IsPrime(int num)
+{
+    if(num <2)
+    {
+        return 0;
+    }
+    for(int i = 2; i <= Math.Sqrt(num); i++)
+    {
+        if(num%i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+List<int> list = new List<int>();
+for(int i = 0; i < A; i++)
+{
+    if(IsPrime(i) == 1)
+    { list.Add(i); }
+}
+
+for(int i = 0;i < list.Count;i++)
+{
+    Console.WriteLine(list[i]);
+}
