c8a3fb3b583ff23016a62c4cd20c9c6212efdbc0
raj0987-alt <raj.tazimul2011@gmail.com>
2024-02-16 09:51:56 +0600

    Prime Number

---
+++ FizzBuzz/PrimeNumberSieve/PrimeNumberSieve/Program.cs
@@ -0,0 +1,38 @@
+﻿int A = 100;
+List<int> list = new List<int>();
+for(int i  = 0; i <= A; i++)
+{
+    if(i<2)
+    {
+        list.Add(0);
+    }
+    else
+    {
+        list.Add(1);
+    }
+    
+}
+for(int i=2; i < list.Count; i++)
+{
+    if (list[i] != 1)
+    {
+        continue;
+    }
+    for (int j = i + i; j < list.Count; j += i)
+    {
+        list[j] = 0;
+    }
+}
+List<int> ss = new List<int>();
+for(int i = 0; i < list.Count; i++)
+{
+    if (list[i] == 1)
+    {
+        ss.Add(i);
+    }
+}
+
+for (int i = 0;i<ss.Count;i++)
+{
+    Console.WriteLine(i+": "+ss[i]);
+}
