2689b290957ff7ff714ff9a54275d8020faf9f6a
raj0987-alt <raj.tazimul2011@gmail.com>
2024-02-17 21:31:25 +0600

    Sells Item

---
+++ Sell items/SellItems/SellItems/Program.cs
@@ -0,0 +1,12 @@
+﻿decimal A = 3;
+decimal B = 17;
+
+var days = 5;
+
+var TotalSell = (B) / A;
+
+var result = (TotalSell / days);
+var d = Math.Ceiling(result);
+var r = (int)d;
+Console.WriteLine(TotalSell);
+Console.WriteLine(r);
