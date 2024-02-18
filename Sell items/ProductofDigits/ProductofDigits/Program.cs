eb31862bb3e702b3530fb6960a3d4587cdf0ddad
raj0987-alt <raj.tazimul2011@gmail.com>
2024-02-18 09:36:13 +0600

    Product of Digits

---
+++ Sell items/ProductofDigits/ProductofDigits/Program.cs
@@ -0,0 +1,19 @@
+﻿int A = 52;
+string S = A.ToString();
+int result = 1;
+int d = 0;
+List<string> list = new List<string>();
+for (int i = 0; i < S.Length; i++)
+{
+    list.Add(S[i].ToString());
+
+
+
+}
+int da = 0;
+for(int i = 0;i < list.Count;i++)
+{
+    da= Int32.Parse(list[i]);
+    result *= da;
+}
+Console.WriteLine(result);
