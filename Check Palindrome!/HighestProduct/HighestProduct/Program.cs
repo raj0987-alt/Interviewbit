ca97bd6c4b128aa5022e2d167c88909ee20f31f9
raj0987-alt <raj.tazimul2011@gmail.com>
2024-02-20 22:43:48 +0600

    Highest Product

---
+++ Check Palindrome!/HighestProduct/HighestProduct/Program.cs
@@ -0,0 +1,15 @@
+﻿List<int> A = new List<int>();
+
+A.Add(4);
+A.Add(5);
+A.Add(1);
+A.Add(2);
+A.Add(3);
+
+A.Sort();
+int n = A.Count;
+
+int m1 = A[n - 1] * A[n - 2] * A[n-3];
+int m2 = A[0] * A[1] * A[n - 1];
+
+Console.WriteLine(Math.Max(m1, m2));
