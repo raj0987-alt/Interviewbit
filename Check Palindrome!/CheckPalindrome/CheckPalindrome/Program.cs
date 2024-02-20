e908e5ce43583187c1bde1cd1d9b93584fae154a
raj0987-alt <raj.tazimul2011@gmail.com>
2024-02-20 21:03:01 +0600

    Pick From Both Sides

---
+++ Check Palindrome!/CheckPalindrome/CheckPalindrome/Program.cs
@@ -0,0 +1,22 @@
+﻿int A = 4;
+
+if (A >= 3 && A <= 5)
+{
+    return "Spring.";
+}
+else if (A >= 6 && A <= 8)
+{
+    return "Summer.";
+}
+else if (A >= 9 && A <= 11)
+{
+    return "Autumn.";
+}
+else if (A == 12 || A == 1 || A == 2)
+{
+    return  "Winter.";
+}
+else
+{
+    return  "Invalid.";
+}
---
+++ Check Palindrome!/PickFromBothSides/PickFromBothSides/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+List<int> A = new List<int>();
+A.Add(5);
+A.Add(-2);
+A.Add(3);
+A.Add(1);
+A.Add(2);
+
+int B = 3;
+
+int max_sum = 0;
+int current_sum = 0;
+
+for(int i = 0;i<B;i++)
+{
+    current_sum += A[i];
+}
+max_sum = current_sum;
+
+for (int i = B - 1, j = A.Count - 1; i >= 0; i--, j--)
+{
+    current_sum = current_sum - A[i] + A[j];
+    max_sum = Math.Max(max_sum, current_sum);
+}
+
+
+Console.WriteLine(max_sum);
