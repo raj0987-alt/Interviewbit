8ad152c7df9d12e6b6732f53271bfae1903ebddf
raj0987-alt <raj.tazimul2011@gmail.com>
2024-02-21 23:56:19 +0600

    Folder Syncronize4

---
+++ Perfect Pick of Array/PerfectPickofArray/PerfectPickofArray/Program.cs
@@ -0,0 +1,19 @@
+﻿List<int> A = new List<int>()
+
+int IsPerfect(List<int> D, int v)
+{
+    for(int j = 0; j < v;j++)
+    {
+        if (D[j] >=A[v])
+        {
+            return 0;
+        }
+    }
+    for(int j = v+1; j < D.Count; j++) { 
+        if (D[j] <= A[v])
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
