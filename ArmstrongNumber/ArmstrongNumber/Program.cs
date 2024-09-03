4e436d89261076b886530ad1d5d857a394f77e5a
raj0987-alt <raj.tazimul2011@gmail.com>
2024-09-03 23:57:32 +0600

    Armstrong Number

---
+++ ArmstrongNumber/ArmstrongNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿int solve(int A)
+{
+    int result = 0;
+    var str = A.ToString();
+    int len = str.Length;
+    for (int i = 0; i < len; i++)
+    {
+
+        int digit = str[i] - '0';
+        result += (int)Math.Pow(digit, len);
+    }
+    if (result == A)
+    {
+        return 1;  // Armstrong number
+    }
+    else
+    {
+        return 0;  // Not an Armstrong number
+    }
+}
+Console.WriteLine(solve(371));
