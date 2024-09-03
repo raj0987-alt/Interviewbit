4a4049c792057dabf7ca1e553174fdbda23c3a1d
raj0987-alt <raj.tazimul2011@gmail.com>
2024-09-03 23:56:08 +0600

    Odd Even Rule

---
+++ OddEvenRule/OddEvenRule/Program.cs
@@ -0,0 +1,22 @@
+﻿int solve(List<int> A, int B, int C)
+{
+    int result = 0;
+    int day = B % 2;
+    for (int i = 0; i < A.Count; i++)
+    {
+        if (A[i] % 2 != day)
+        {
+            result++;
+        }
+    }
+    return result * C;
+}
+
+List<int> list = new List<int>();
+//list.Add(0);
+list.Add(1);
+list.Add(2);
+list.Add(3);
+int B = 31;
+int C = 100;
+Console.WriteLine(solve(list, B, C));
