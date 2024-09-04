b9fb87b0a9f3cc71cf6e14c3ada3e836db0169c3
raj0987-alt <raj.tazimul2011@gmail.com>
2024-09-04 23:38:58 +0600

    Socks Pair

---
+++ GreaterofLesser/GreaterofLesser/Solution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreaterofLesser
+{
+    internal class Solution
+    {
+        public int solve(List<int> A, List<int> B, int C)
+        {
+            int r = 0;
+            int g = 0;
+            int l = 0;
+            for (int i = 0; i < A.Count; i++)
+            {
+                if (A[i] > C)
+                {
+                    g++;
+                }
+            }
+            for (int i = 0; i < B.Count; i++)
+            {
+                if (B[i] < C)
+                {
+                    l++;
+                }
+            }
+            if (g > l)
+            {
+                return g;
+            }
+            else
+            {
+                return l;
+            }
+        }
+    }
+}
---
+++ SocksPair/SocksPair/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+ int solve(List<int> A)
+{
+    int result = 0;
+
+    A.Sort();
+    int i = 0;
+    while (i< A.Count-1)
+    {
+        if (A[i] == A[i+1])
+        {
+            result++;
+            i += 2;
+        }
+        else
+        {
+            i++; // Move to the next element
+        }
+    }
+    return result;
+}
+List<int> B = new List<int>();
+B.Add(2);
+B.Add(2);
+B.Add(2);
+B.Add(2);
+Console.WriteLine(solve(B));
