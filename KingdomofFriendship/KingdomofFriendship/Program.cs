c80fdad96955fe03bf195858a6738a5053616561
raj0987-alt <raj.tazimul2011@gmail.com>
2024-09-06 20:23:05 +0600

    Kingdom Of FriendShip

---
+++ KingdomofFriendship/KingdomofFriendship/Program.cs
@@ -0,0 +1,19 @@
+﻿class Solution
+{
+    public int solve(List<int> A)
+    {
+        int count = 0;
+        int N = A.Count;
+
+        for (int i = 0; i < N; i++)
+        {
+            int j = A[i] - 1;  // Person i's best friend (0-indexed)
+            if (i != j && A[j] - 1 == i)
+            {
+                count++;
+            }
+        }
+
+        return count / 2;
+    }
+}
