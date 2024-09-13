5fc4b04c175256c4caa78613760b298fbaea07db
raj0987-alt <raj.tazimul2011@gmail.com>
2024-09-13 23:14:14 +0600

    Frequency of Characters

---
+++ FrequencyofCharacters/FrequencyofCharacters/Program.cs
@@ -0,0 +1,45 @@
+﻿//using FrequencyofCharacters;
+//string data = "asdfrea";
+
+
+
+//Solver solver = new Solver();
+//solver.CalculateVal(data);
+//foreach(var i in solver.ints)
+//{
+//    Console.WriteLine(i);
+//}
+////Console.WriteLine(solver.ints);
+///
+
+
+
+using FrequencyofCharacters;
+///
+List<int> solve(string A)
+{
+    List<int> ints = new List<int>()
+{
+    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+};
+
+    char[] charArray = A.ToCharArray();
+    foreach (char ch in charArray)
+    {
+        if (ch >= 'a' && ch <= 'z')
+        {
+            int index = ch - 'a';  // Get the position in the alphabet
+            ints[index]++;
+        }
+    }
+
+
+
+    return ints;
+}
+solve("TAZIMUL");
+
+
+//public List<int> solve(string A)
+//{
+//}
---
+++ FrequencyofCharacters/FrequencyofCharacters/Solver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrequencyofCharacters
+{
+    internal class Solver
+    {
+        public List<int> ints = new List<int>()
+        {
+            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        };
+
+        public void CalculateVal(string data)
+        {
+            char[] charArray = data.ToCharArray();
+            foreach (char ch in charArray)
+            {
+                if (ch >= 'a' && ch <= 'z')
+                {
+                    int index = ch - 'a';  // Get the position in the alphabet
+                    ints[index]++;
+                }
+            }
+
+            
+        }
+
+
+    }
+}
